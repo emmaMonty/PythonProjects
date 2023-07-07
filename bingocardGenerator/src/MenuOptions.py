ff1afae20ffbce0deaacafc6a6b732c776452288
emmaMonty <montgomery.emma0710@gmail.com>
2023-07-07 14:46:30 -0600

    adding menuoptions.py

---
+++ bingocardGenerator/src/MenuOptions.py
@@ -0,0 +1,10 @@
+class MenuOption():
+    def __init__(self, command, description):
+        self.__m_command = command
+        self.__m_description = description
+
+    def getCommand(self):
+        return self.__m_command
+
+    def getDescription(self):
+        return self.__m_description
